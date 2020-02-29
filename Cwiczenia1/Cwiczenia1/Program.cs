7ef767881e6130fea47dd444e54ac5b651315123
s17722 <s17722@pjwstk.edu.pl>
2020-02-29 19:38:23 +0100

    New console app project

---
+++ Cwiczenia1/Cwiczenia1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Cwiczenia1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            
+            
+            var client = new HttpClient(); //HttpClient client = new HttpClient();
+            var result = await client.GetAsync("https://www.pja.edu.pl");
+
+            if (result.IsSuccessStatusCode) //2xx
+            {
+                string html = await result.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+
+                MatchCollection matches = regex.Matches(html);
+
+                foreach(var m in matches)
+                {
+                    Console.WriteLine(m.ToString());
+                }
+
+            }
+
+            Console.WriteLine("Koniec!");
+
+        }
+    }
+}
